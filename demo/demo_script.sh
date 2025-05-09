c2a021a15286e479c43feb5884d351eb47f66e7a
Rui Coelho <ruicoelho@ua.pt>
2025-05-09 23:25:31 +0100

    Application demo

---
+++ demo/demo_script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo "kontext ns --help"
+kontext ns -h
+sleep 2
+clear
+echo "kontext"
+kontext
+sleep 2
+clear
+echo "kontext ns"
+kontext ns
+sleep 2
+clear
+echo "kontext -n"
+kontext -n
+sleep 2
+clear
+echo "kontext ns -s"
+kontext ns -s
